9d3b8f1b96fc08121afcdd5e5de577b156ed6a6c
NeoCentrum <neo.centrum@gmail.com>
2015-02-08 12:15:24 -0800

    updated comments and explanations

--- get_plot_dataset.R
+++ get_plot_dataset.R
@@ -1,32 +1,45 @@
-getPlotDataSet <- function(skipr=1, strow = 0, edrow = 0){
+#GETTING SUBSET OF DATA
+#We use this file for utility functions for extracting subset of Data from the Large Data set.
+
+#function: getPlotDataSet(...) is used to get the subset of Data from the Lareg Dateset, this helps with optmial memory
+#                         utilization because we do not need to store the whole data. in addition, the function can
+#                         is designed to run a trace to get data only for dates 1/2/2007 and 2/2/2007, if run without
+#                         parameters, it will take longer but will provide the required dataset, since I could run it 
+#                         I have found where the data exactly is and thus we can use exact data start and end points to
+#                         quickly get the data. Another important aspect is that for a uniform data ,we culd have found
+#                         start and end mathematically using the sample data finding the data collection interval.
+#Parameters: startdate : target extraction start date, in this case : "1/2/2007 00:00:00" - (dd/mm/yyyy hh:mm:ss)
+#            enddate : target extraction end date, in this case : "2/2/2007 23:59:00" - (dd/mm/yyyy hh:mm:ss)       
+#            skipr: skip rows can be used to skip specific number of rows
+#            strow : start row, where to start extraction from.
+#            edrow : end row, where to end extraction. helps quickly get the data.
+#Returns : dataset containing extracted data 
+getPlotDataSet <- function(startdate = "1/2/2007 00:00:00", enddate = "2/2/2007 23:59:00",skipr=1, strow = 0, edrow = 0){
+  #Flag for terminating trace loop for data.
   datasetfound <- FALSE
   skiprows <- skipr
   #startrow<-66637
   startrow <- strow
   #endrow <- 69516
   endrow<- edrow
+  #Lopp through Data one row at a time, till we get to startdate and then enddate.
   while(datasetfound == FALSE && endrow == 0) {
     record <- read.table("household_power_consumption.txt", header = FALSE, sep = ";",quote = "\"'",na.strings = "?",col.names = c("Date","Time","Global_active_power","Global_reactive_power","Voltage","Global_intensity","Sub_metering_1","Sub_metering_2","Sub_metering_3"),nrows = 1, skip = skiprows)
-    startTime <- strptime("1/2/2007 00:00:00",format = "%d/%m/%Y %H:%M:%S")
-    endTime <- strptime("2/2/2007 23:59:00",format = "%d/%m/%Y %H:%M:%S")  
+    startTime <- strptime(startdate,format = "%d/%m/%Y %H:%M:%S")
+    endTime <- strptime(enddate,format = "%d/%m/%Y %H:%M:%S")  
     recTime <- strptime(paste(record[[1]],record[[2]],sep=" "),format = "%d/%m/%Y %H:%M:%S")
     
     if(recTime >= startTime && (startrow == 0)){
       startrow = skiprows
-      #print(record[1])
       
     } else if(recTime >= endTime && (endrow == 0)){
       endrow <- skiprows
       datasetfound <- TRUE
-      #print(record[1]) 
       break;
     }
     skiprows <- skiprows + 1
-    #print(record[1])
-    #print(skiprows) 
   }
+  #extract data for the given startdate -> enddate range.
   datast <- read.table("household_power_consumption.txt", header = FALSE, sep = ";",quote = "\"'",na.strings = "?",col.names = c("Date","Time","Global_active_power","Global_reactive_power","Voltage","Global_intensity","Sub_metering_1","Sub_metering_2","Sub_metering_3"),nrows = (endrow - startrow), skip = startrow)
-  #print(length(datast))
-  #print(startrow)
-  #print(endrow)
+
 }
--- plot1.R
+++ plot1.R
@@ -1,9 +1,33 @@
+#PLOT1 from the Data.
+#We use this file for plot1 plotting functions from extracted subset of Data from the Large Data set.
+
+
+#We use get_plot_dataset.R for the extraction utility function.
+#function: getPlotDataSet(...) is used to get the subset of Data from the Lareg Dateset, this helps with optmial memory
+#                         utilization because we do not need to store the whole data. in addition, the function can
+#                         is designed to run a trace to get data only for dates 1/2/2007 and 2/2/2007, if run without
+#                         parameters, it will take longer but will provide the required dataset, since I could run it 
+#                         I have found where the data exactly is and thus we can use exact data start and end points to
+#                         quickly get the data. Another important aspect is that for a uniform data ,we culd have found
+#                         start and end mathematically using the sample data finding the data collection interval.
+#Parameters: startdate : target extraction start date, in this case : "1/2/2007 00:00:00" - (dd/mm/yyyy hh:mm:ss)
+#            enddate : target extraction end date, in this case : "2/2/2007 23:59:00" - (dd/mm/yyyy hh:mm:ss)       
+#            skipr: skip rows can be used to skip specific number of rows
+#            strow : start row, where to start extraction from.
+#            edrow : end row, where to end extraction. helps quickly get the data.
+#Returns : dataset containing extracted data 
 source('get_plot_dataset.R')
+
+#function: plot1() is used to data for the Global Active Power values.
+#Returns : png plot of the datasert
 plot1 <- function(){
+  #get dataset for plot generation.
   plotdata <- getPlotDataSet(66637, 66637, 69516)
+  #set margins
   par(mar = c(5,5,2,2))
-  #mfrow(1,1)
+  #plot data
   with(plotdata,hist(Global_active_power, col="red", main = "Global Active Power", xlab="Global Active Power(kilowatts)", ylab="Frequency")) 
+  #send to png device
   dev.copy(png, file="plot1.png")
   dev.off()
 }
